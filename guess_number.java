e3ae93b08ccea4c0719caa0fbab297e88ce7b5e2
mamenjivar <menjivarmiguel221@gmail.com>
2015-08-12 23:27:26 -0700

    need to loop until correct number guessed

--- guess_number.java
+++ guess_number.java
@@ -1,13 +1,23 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class guess_number{
 	public static void main(String args[]){
 
+		Random randomNumbers = new Random();
 		Scanner keyboard = new Scanner(System.in);
-		String number;
-		System.out.println("Please enter a number from 1 to 10: ");
+
+		int number;
+		int number1 = 5;
+		System.out.print("Please enter a number from 1 to 10: ");
 		number = keyboard.nextInt();
 
-		System.out.println(number);
+		//number1 = randomNumbers.nextInt(10) + 1;
+
+		if (number == number1){
+			System.out.println("Correct number!");
+		} else{
+			System.out.println("wrong number!");
+		}
 	}
 }
