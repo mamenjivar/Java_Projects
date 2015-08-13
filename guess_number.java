1160d2da2968878728237fc25e96fdeea4e04a82
mamenjivar <menjivarmiguel221@gmail.com>
2015-08-13 14:24:49 -0700

    fully functional

--- guess_number.java
+++ guess_number.java
@@ -7,17 +7,18 @@ public class guess_number{
 		Random randomNumbers = new Random();
 		Scanner keyboard = new Scanner(System.in);
 
-		int number;
-		int number1 = 5;
+		int number; //variable for keyboard input
+		int number1; //variable for random number
+
 		System.out.print("Please enter a number from 1 to 10: ");
 		number = keyboard.nextInt();
 
-		//number1 = randomNumbers.nextInt(10) + 1;
-
-		if (number == number1){
-			System.out.println("Correct number!");
-		} else{
-			System.out.println("wrong number!");
+		number1 = randomNumbers.nextInt(10) + 1; //random number range from 1-10
+	
+		while (number != number1){ //keeps looping until correct answer is reached
+			System.out.print("wrong number, please try again: ");
+			number = keyboard.nextInt();
 		}
+		System.out.println("You guessed the correct number!");
 	}
 }
