9c4da564552f00308a3355c4a9002419c94686d4
mamenjivar <Miguel@macs-MBP.att.net>
2015-08-13 17:47:57 -0700

    guessing game with hints and counters

--- guess_number.java
+++ guess_number.java
@@ -1,4 +1,6 @@
-import java.util.Scanner;
+//guessing game with hints
+
+import java.util.Scanner; 
 import java.util.Random;
 
 public class guess_number{
@@ -8,25 +10,30 @@ public class guess_number{
 		Scanner keyboard = new Scanner(System.in);
 
 		int number; //variable for keyboard input
-		int number1; //variable for random number
+		int rand_num; //variable for random number
+		int tracker = 1;
 
-		System.out.print("Please enter a number from 1 to 10: ");
+		System.out.print("Please enter a number from 1 through 10: ");
 		number = keyboard.nextInt();
 
-		number1 = randomNumbers.nextInt(10) + 1; //random number range from 1-10
+		rand_num = randomNumbers.nextInt(10) + 1; //random number range from 1-10
 		
-		while (number != number1){ //keeps looping until correct answer is reached
+		while (number != rand_num){ //keeps looping until correct answer is reached
 			if (number < 0 || number > 10){ //checks if number is outside of 1-10 range
 				System.out.print("Pick a number between 1 and 10: ");
 				number = keyboard.nextInt();
-			} else if (number1 < number){ //checks value of number if higher than guessed number
+				tracker++;
+			} else if (rand_num < number){ //checks value of number if higher than guessed number
 				System.out.print("Hint: go lower ");
 				number = keyboard.nextInt();
-			} else if (number1 > number){ //checks value of number if lower than guess number
+				tracker++;
+			} else if (rand_num > number){ //checks value of number if lower than guess number
 				System.out.print("Hint: go higher ");
 				number = keyboard.nextInt();
+				tracker++;
 			}		
 		}
 		System.out.println("You guessed the correct number!");
+		System.out.println("It took you " + tracker + " tries!");
 	}
 }
