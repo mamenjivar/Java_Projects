01827a3530d0930a2e77f5acc0af714539a66cc8
mamenjivar <menjivarmiguel221@gmail.com>
2015-08-15 15:35:11 -0700

    validates if a word is a pallindrome

--- pallindrome.java
+++ pallindrome.java
@@ -6,24 +6,24 @@ public class pallindrome{
 	public static void main(String args[]){
 
 		Scanner keyboard = new Scanner(System.in);
-		String pallindrome;
+		String pallindrome; 
 		char repeat;
 		String input;
 
 		do { 
-			System.out.print("Please enter a pallindrome: ");
-			pallindrome = keyboard.nextLine();
+			System.out.print("Please enter a pallindrome: "); //keyboard input of pallindrome
+			pallindrome = keyboard.nextLine(); 
 
-			String reverse = new StringBuilder(pallindrome).reverse().toString();
+			String reverse = new StringBuilder(pallindrome).reverse().toString(); //reverses word and converts to a string
 
-			if(pallindrome.equals(reverse)) {
-				System.out.println("It is a pallindrome: ");
+			if(pallindrome.equals(reverse)) { //if original value equals reversed value
+				System.out.println("It is a pallindrome: "); //two values are equal
 			} else{
-				System.out.println("it is not a pallindrome: ");
+				System.out.println("it is not a pallindrome: "); //two values are not equal
 			}
 			System.out.print("Would you like to try another word? ");
 			input = keyboard.nextLine();
 			repeat = input.charAt(0);
-		} while (repeat == 'Y' || repeat =='y');
+		} while (repeat == 'Y' || repeat =='y'); //prompts user if they want to try another word
 	}
 }
