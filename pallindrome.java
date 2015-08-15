252124140ba49c32964c303dd9fec4b3bc55eb5b
mamenjivar <menjivarmiguel221@gmail.com>
2015-08-15 12:22:00 -0700

    checks if a word is a pallindrome

--- pallindrome.java
+++ pallindrome.java
@@ -1,28 +1,29 @@
 //program will check to see if inputted word is a pallindrome
 
 import java.util.Scanner;
-import java.util.Collections;
 
 public class pallindrome{
 	public static void main(String args[]){
 
 		Scanner keyboard = new Scanner(System.in);
 		String pallindrome;
-		String copy;
+		char repeat;
+		String input;
 
-		System.out.print("Please enter a pallindrome: ");
-		pallindrome = keyboard.nextLine();
+		do { 
+			System.out.print("Please enter a pallindrome: ");
+			pallindrome = keyboard.nextLine();
 
-		StringBuilder a = new StringBuilder(pallindrome);
-		System.out.println(a);
-		System.out.println(a.reverse());
-		//String b = a.reverse();
-		//String c = b.toString();
+			String reverse = new StringBuilder(pallindrome).reverse().toString();
 
-		if(a == a.reverse()) {
-			System.out.println("It is a pallindrome: ");
-		} else{
-			System.out.println("it is not a pallindrome: ");
-		}
+			if(pallindrome.equals(reverse)) {
+				System.out.println("It is a pallindrome: ");
+			} else{
+				System.out.println("it is not a pallindrome: ");
+			}
+			System.out.print("Would you like to try another word? ");
+			input = keyboard.nextLine();
+			repeat = input.charAt(0);
+		} while (repeat == 'Y' || repeat =='y');
 	}
 }
