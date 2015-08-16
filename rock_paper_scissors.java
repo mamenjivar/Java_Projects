3cc853b2af425b804ad1a12cee8d185b09c31fb0
mamenjivar <Miguel@macs-MBP.att.net>
2015-08-15 20:34:54 -0700

    added switch statement

--- rock_paper_scissors.java
+++ rock_paper_scissors.java
@@ -11,14 +11,29 @@ public class rock_paper_scissors{
 
 		int roshambo;
 		int rand_num;
-		rock(1);
-		paper(2);
-		scissors(3);
+		//rock(1);
+		//paper(2);
+		//scissors(3);
 
-		System.out.print("rock, paper, scissors: ");
+		System.out.println("Welcome to the game of rock, paper scissors!");
+		System.out.print("1 = rock, 2 = paper, 3 = scissors");
 		roshambo = keyboard.nextInt();
 
 		rand_num = randomNumbers.nextInt(3) + 1;
+
+		switch (roshambo) {
+			case 1:
+				System.out.println("you entered 1");
+				break;
+			case 2:
+				System.out.println("You entered 2");
+				break;
+			case 3:
+				System.out.println("You entered 3");
+				break;
+			default:
+				System.out.print("choose rock, paper, or scissors");
+		}
 		/*
 		rock = 1
 		paper = 2
