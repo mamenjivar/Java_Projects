27542745f4fa0d82448f78297d6c4080d2ad53a2
mamenjivar <menjivarmiguel221@gmail.com>
2015-08-17 13:41:51 -0700

    simple game of rock, paper, and scissors against a computer with point tracker

--- rock_paper_scissors.java
+++ rock_paper_scissors.java
@@ -1,4 +1,4 @@
-//simple game of rock paper and scissors
+//simple game of rock paper and scissors with point tracker
 
 import java.util.Scanner;
 import java.util.Random;
@@ -9,67 +9,83 @@ public class rock_paper_scissors{
 		Scanner keyboard = new Scanner(System.in);
 		Random randomNumbers = new Random();
 
-		int roshambo;
-		int rand_num;
-		String input;
+		int roshambo; //user input
+		int rand_num; //computer input
+		String input; 
 		char repeat;
+		/*counters for win and loss*/
 		int win = 0;
 		int loss = 0;
+		int draw = 0;
 
 		System.out.println("Welcome to the game of rock, paper, and scissors!");
 		do {
-			System.out.println("1 = rock, 2 = paper, 3 = scissors");
+			System.out.println("1 = rock, 2 = paper, 3 = scissors"); //choices for r, p, s
 			System.out.print("Please refer to the menu above: ");
 			roshambo = keyboard.nextInt();
 			keyboard.nextLine();
 
-			rand_num = randomNumbers.nextInt(3) + 1;
+			rand_num = randomNumbers.nextInt(3) + 1; //randomizes computer choices with 1, 2, and 3
 			/*user choices*/
 			switch (roshambo) {
 				case 1: //rock
 					rock(rand_num);
+					/*if else statement adds a point to computer or user based on winning or losing*/
 					if (rand_num == 2){
 						loss++;
 					} else if (rand_num == 3) {
 						win++;
+					} else {
+						draw++;
 					}
 					break;
 				case 2: //paper
 					paper(rand_num);
+					/*if else statement adds a point to computer or user based on winning or losing*/
 					if (rand_num == 3){
 						loss++;
 					} else if (rand_num == 1) {
 						win++;
+					} else {
+						draw++;
 					}
 					break;
 				case 3://scissors
 					scissors(rand_num);
+					/*if else statement adds a point to computer or user based on winning or losing*/
 					if (rand_num == 1){
 						loss++;
 					} else if (rand_num == 2) {
 						win++;
+					} else{
+						draw++;
 					}
 					break;
 				default:
 					System.out.print("choose rock, paper, or scissors");
 			}
+			/*prompts user if they want to play again*/
 			System.out.print("Would you like to try again? ");
 			input = keyboard.nextLine();
 			repeat = input.charAt(0);
 		} while (repeat == 'Y' || repeat == 'y');
 
-		System.out.println("Final Score: You " + win + " Computer " + loss);
+		System.out.println("Final Score =  You: " + win + " Draws: " + draw + " Computer: " + loss); //after program quits, user is prompted with final score
 	} 	/*computer choices*/
 		/*user outcome for rock*/
 		public static void rock(int num){
+
 			switch (num) {
 			case 1: //rock
-				System.out.println("It is a draw");
+				System.out.println("Computer chose rock");
+				System.out.println("It is a draw!");
 				break;
 			case 2: //paper
+				System.out.println("Computer chose paper");
 				System.out.println("You lost!");
 				break;
 			case 3: //scissors
+				System.out.println("Computer chose scissors");
 				System.out.println("You Win!");
 				break;
 			default:
@@ -80,12 +96,15 @@ public class rock_paper_scissors{
 		public static void paper(int num){
 			switch (num) {
 				case 1: // rock
+					System.out.println("Computer chose rock");
 					System.out.println("You Win!");
 					break;
 				case 2: // paper
+					System.out.println("Computer chose paper");
 					System.out.println("It is a draw!");
 					break;
 				case 3: // scissors
+					System.out.println("Computer chose scissors");
 					System.out.println("You Lost!");
 					break;
 				default:
@@ -96,13 +115,16 @@ public class rock_paper_scissors{
 		public static void scissors(int num){
 			switch (num) {
 				case 1: //rock
+					System.out.println("Computer chose rock");
 					System.out.println("You Lost!");
 					break;
 				case 2: //paper
+					System.out.println("Computer chose paper");
 					System.out.println("You Win!");
 					break;
 				case 3: //scissors
-					System.out.println("It is a Draw");
+					System.out.println("Computer chose scissors");
+					System.out.println("It is a draw!");
 					break;
 				default:
 					System.out.print("choose rock, paper, or scissors");
