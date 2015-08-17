3d6dcbe522614e311f12efe34495b56d03ec5627
mamenjivar <menjivarmiguel221@gmail.com>
2015-08-17 00:08:51 -0700

    functional program, needs loop function

--- rock_paper_scissors.java
+++ rock_paper_scissors.java
@@ -11,49 +11,82 @@ public class rock_paper_scissors{
 
 		int roshambo;
 		int rand_num;
-		//rock(1);
-		//paper(2);
-		//scissors(3);
+		String choice;
+		char repeat;
 
-		System.out.println("Welcome to the game of rock, paper scissors!");
-		System.out.print("1 = rock, 2 = paper, 3 = scissors");
-		roshambo = keyboard.nextInt();
+		System.out.println("Welcome to the game of rock, paper, and scissors!");
+		do {
+			System.out.println("1 = rock, 2 = paper, 3 = scissors");
+			System.out.print("Please refer to the menu above: ");
+			roshambo = keyboard.nextInt();
 
-		rand_num = randomNumbers.nextInt(3) + 1;
+			rand_num = randomNumbers.nextInt(3) + 1;
+			/*user choices*/
+			switch (roshambo) {
+				case 1: //rock
+					rock(rand_num);
+					break;
+				case 2: //paper
+					paper(rand_num);
+					break;
+				case 3://scissors
+					scissors(rand_num);
+					break;
+				default:
+					System.out.print("choose rock, paper, or scissors");
+			}
+			System.out.println("Would you like to try again? ");
+			choice = keyboard.nextLine();
+			repeat = choice.charAt(0);
+		} while (repeat == 'Y' || repeat == 'y');
+	} /*computer choices*/
+		public static void rock(int num){
+			/*user outcome for rock*/
+			switch (num) {
+			case 1: //rock
+				System.out.println("It is a draw");
 
-		switch (roshambo) {
-			case 1:
-				System.out.println("you entered 1");
 				break;
-			case 2:
-				System.out.println("You entered 2");
+			case 2: //paper
+				System.out.println("You lost!");
 				break;
-			case 3:
-				System.out.println("You entered 3");
+			case 3: //scissors
+				System.out.println("You Won!");
 				break;
 			default:
 				System.out.print("choose rock, paper, or scissors");
+			}
 		}
-		/*
-		rock = 1
-		paper = 2
-		scissors = 3
-		use switch statements
-		*/
-		
-		/*public static void rock(){
-			int rock = 1;
-			System.out.print("Computer chose rock");
+		public static void paper(int num){
+			/*user outcome for paper*/
+			switch (num) {
+				case 1: // rock
+					System.out.println("You Win!");
+					break;
+				case 2: // paper
+					System.out.println("It is a draw!");
+					break;
+				case 3: // scissors
+					System.out.println("You Lost!");
+					break;
+				default:
+					System.out.print("choose rock, paper, or scissors");
+			}
 		}
-		public static void paper(){
-			int paper = 2;
-			System.out.print("Computer chose paper");
+		public static void scissors(int num){
+			/*user outcome for scissors*/
+			switch (num) {
+				case 1: //rock
+					System.out.println("You Lost!");
+					break;
+				case 2: //paper
+					System.out.println("You Won!");
+					break;
+				case 3: //scissors
+					System.out.println("It is a Draw");
+					break;
+				default:
+					System.out.print("choose rock, paper, or scissors");
+			}
 		}
-		public static void scissors(){
-			int scissors = 3;
-			System.out.print("Computer chose scissors");
-		}*/
-
-
-	}
 }
