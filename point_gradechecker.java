123e114c40e61189ac9036e4259f4389e5dfcb94
mamenjivar <Miguel@macs-MBP.att.net>
2015-08-17 17:55:39 -0700

    grade checker for point based systems

--- point_gradechecker.java
+++ point_gradechecker.java
@@ -3,28 +3,48 @@
 import java.util.Scanner;
 
 public class point_gradechecker{
-	public static void public static void main(String[] args) {
+	public static void main(String[] args) {
 		 
-		 Scanner keyboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in);
+		/*variables for points earned in assignment*/
+		double score_earned;
+		double score_earned1 = 0;
+		/*variables for maximum points in assignments*/
+		double overal_score;
+		double overal_score1 = 0;
+		/*variable to display grade*/
+		double grade_score = 0;
+		/*variables to ask user if wanting to input more scores*/
+		char repeat;
+		String input;
 
-		 int score_earned = 0;
-		 int overal_score = 0;
-		 int final score;
-		 char repeat;
-		 String input;
-
-		 do {
+		do {
+			//asks user to input scores
 		 	System.out.print("Please enter the points earned for assignment: ");
-		 	score_earned = keyboard.nextInt();
+		 	score_earned = keyboard.nextDouble();
 
+		 	//asks user to enter maximum points allowed on assignment
 		 	System.out.print("Please enter overal points of assignment: ");
-		 	overal_score = keyboard.nextInt();
+		 	overal_score = keyboard.nextDouble();
+		 	keyboard.nextLine();
 
+		 	//asks user if they need to enter more assingments
 		 	System.out.print("Would you like to add another assignment? ");
 		 	input = keyboard.nextLine();
-		 	char = input.charAt(0);
+		 	repeat = input.charAt(0);
+
+		 	/*adds up all points earned from all assignments together*/
+		 	score_earned1 += score_earned;
+		 	/*adds up total points you can get from all assigments together*/
+		 	overal_score1 += overal_score;
+		 	/*calculates the current grade together*/
+		 	grade_score = score_earned1 / overal_score1 * 100;
+
 
 		} while (repeat == 'Y' || repeat == 'y');
-
+		//shows results onto the terminal screen
+		System.out.println("total points earned from points assignment: " + score_earned1);
+		System.out.println("overal points from all assignments: " + overal_score1);
+		System.out.println("Overall grade: " + grade_score);
 	}
 }
