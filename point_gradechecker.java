de0dda104f98d661993ab51fd382f076a4635803
mamenjivar <menjivarmiguel221@gmail.com>
2015-08-24 22:18:41 -0700

    checks overall grade for point system

--- point_gradechecker.java
+++ point_gradechecker.java
@@ -15,13 +15,18 @@ public class point_gradechecker{
 		double overal_score1 = 0;
 		/*variable to display grade*/
 		double grade_score = 0;
-		/*variables to ask user if wanting to input more scores*/
-		char repeat;
-		String input;
+		/*declares the for loop reiterations*/
+		int taco;
+		int number;
 
 		DecimalFormat formatter = new DecimalFormat("#.00");
 
-		do {
+		System.out.print("How many scores do you want to input? ");
+		taco = keyboard.nextInt();
+		keyboard.nextLine();
+
+
+		for (number = 1;  number <= taco; number++) {
 			//asks user to input scores
 		 	System.out.print("Please enter the points earned for assignment: ");
 		 	score_earned = keyboard.nextDouble();
@@ -31,22 +36,17 @@ public class point_gradechecker{
 		 	overal_score = keyboard.nextDouble();
 		 	keyboard.nextLine();
 
-		 	//asks user if they need to enter more assingments
-		 	System.out.print("Would you like to add another assignment? ");
-		 	input = keyboard.nextLine();
-		 	repeat = input.charAt(0);
-
 		 	/*adds up all points earned from all assignments together*/
 		 	score_earned1 += score_earned;
 		 	/*adds up total points you can get from all assigments together*/
 		 	overal_score1 += overal_score;
 		 	/*calculates the current grade together*/
 		 	grade_score = score_earned1 / overal_score1 * 100;
-
-		} while (repeat == 'Y' || repeat == 'y');
+		}
 		//shows results onto the terminal screen
-		System.out.println("total points earned from points assignment: " + score_earned1);
-		System.out.println("overal points from all assignments: " + overal_score1);
+		System.out.println();
+		System.out.println("total points earned from all assignment: " + score_earned1);
+		System.out.println("overall points from all assignments: " + overal_score1);
 		System.out.println("Overall grade: " + formatter.format(grade_score));
 	}
 }
