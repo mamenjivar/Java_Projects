5fcbaaed64ac5eb00c5dc959568b36677cb3451a
mamenjivar <menjivarmiguel221@gmail.com>
2015-08-24 22:20:40 -0700

    calculator under construction

--- simple_calculator.java
+++ simple_calculator.java
@@ -1,8 +1,8 @@
 //simple calculator
 
-public class calculator{
+public class simple_calculator{
 	public static void main(String[] args) {
 		System.out.println("Welcome to a simple calculator");
-		
+
 	}
 }
