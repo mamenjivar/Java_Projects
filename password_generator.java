f6b31a491fac9bac39b86abff9ee953e4509dbb5
mamenjivar <menjivarmiguel221@gmail.com>
2018-03-14 00:30:46 -0700

    updating folder with new programs

---
+++ password_generator.java
@@ -0,0 +1,17 @@
+//going to loop
+//have classes to split up each character
+//include numbers, letters, and special characters
+import java.util.Scanner;
+import java.util.Random;
+
+public class password_generator{
+  public static void main(String[] args){
+
+    Scanner keyboard = new keyboard(System.in);
+    Random random_numbers = new Random();
+    
+    system.out.println("Welcome to the password generator");
+
+
+  }
+}
